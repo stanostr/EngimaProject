87fa8fc87b2e6c2944ac0711a96883186e0da1ed
Stan <stanislav.ostrovskii10@my.stjohns.edu>
2017-11-04 15:01:34 -0400

    The final version of the Enigma machine. Hopefully!
    Now we can use it for trying different algorithms for breaking the code.

--- src/Enigma.java
+++ src/Enigma.java
@@ -26,6 +26,16 @@ public class Enigma{
 	static int[] Rotor8Int = new int[26];
 	static int[] PlugInt = new int[26];
 	static int[] ReflectorInt = new int[26];
+	
+	static int[] Rotor1Turn = new int[]{16,16};
+	static int[] Rotor2Turn = new int[]{4,4};
+	static int[] Rotor3Turn = new int[]{21,21};
+	static int[] Rotor4Turn = new int[]{9,9};
+	static int[] Rotor5Turn = new int[]{25,25};
+	static int[] Rotor6Turn = new int[]{12,25};
+	static int[] Rotor7Turn = new int[]{12,25};
+	static int[] Rotor8Turn = new int[]{12,25};
+	
 
 /**
  * for this debugger enter the input as follows in console:
@@ -69,98 +79,132 @@ public static String enc(int[] settings, String inputString)
 	int middleGround = settings[7];
 	int rightGround = settings[8];
 	
-	if(leftGround-leftRing >= 0)leftGround -=leftRing;
-	else leftGround = 26+(leftGround-leftRing);
-	System.out.println("leftground: "+ leftGround);
-	if(middleGround-middleRing >= 0) middleGround -= middleRing;
-	else middleGround = 26+(middleGround-middleRing);
-	System.out.println("middleground: " + middleGround);
-	if(rightGround-rightRing >= 0)rightGround -= rightRing;
-	else rightGround = 26+(rightGround-rightRing);
-	System.out.println("rightground: " +rightGround);
+	int[] rightRotorArray;
+	int[] middleRotorArray;
+	int[] leftRotorArray;
 	
+	if(rightRotor==1) rightRotorArray = Rotor1Int; 
+	else if(rightRotor==2) rightRotorArray = Rotor2Int;
+	else if(rightRotor==3) rightRotorArray = Rotor3Int;
+	else if(rightRotor==4) rightRotorArray = Rotor4Int;
+	else if(rightRotor==5) rightRotorArray = Rotor5Int;
+	else if(rightRotor==6) rightRotorArray = Rotor6Int;
+	else if(rightRotor==7) rightRotorArray = Rotor7Int;
+	else rightRotorArray = Rotor8Int;
+	
+	if(middleRotor==1) middleRotorArray = Rotor1Int; 
+	else if(middleRotor==2) middleRotorArray = Rotor2Int;
+	else if(middleRotor==3) middleRotorArray = Rotor3Int;
+	else if(middleRotor==4) middleRotorArray = Rotor4Int;
+	else if(middleRotor==5) middleRotorArray = Rotor5Int;
+	else if(middleRotor==6) middleRotorArray = Rotor6Int;
+	else if(middleRotor==7) middleRotorArray = Rotor7Int;
+	else middleRotorArray = Rotor8Int;
+	
+	if(leftRotor==1) leftRotorArray = Rotor1Int; 
+	else if(leftRotor==2) leftRotorArray = Rotor2Int;
+	else if(leftRotor==3) leftRotorArray = Rotor3Int;
+	else if(leftRotor==4) leftRotorArray = Rotor4Int;
+	else if(leftRotor==5) leftRotorArray = Rotor5Int;
+	else if(leftRotor==6) leftRotorArray = Rotor6Int;
+	else if(leftRotor==7) leftRotorArray = Rotor7Int;
+	else leftRotorArray = Rotor8Int;
+	
+	//determines the spots where the rotors will the turn the next one
+	int middleRotorTurn[];
+	int rightRotorTurn[];
+	
+	if(middleRotor==1) middleRotorTurn = Rotor1Turn; 
+	else if(middleRotor==2) middleRotorTurn = Rotor2Turn;
+	else if(middleRotor==3) middleRotorTurn = Rotor3Turn;
+	else if(middleRotor==4) middleRotorTurn = Rotor4Turn;
+	else if(middleRotor==5) middleRotorTurn = Rotor5Turn;
+	else if(middleRotor==6) middleRotorTurn = Rotor6Turn;
+	else if(middleRotor==7) middleRotorTurn = Rotor7Turn;
+	else middleRotorTurn = Rotor8Turn;
+	
+	if(rightRotor==1) rightRotorTurn = Rotor1Turn; 
+	else if(rightRotor==2) rightRotorTurn = Rotor2Turn;
+	else if(rightRotor==3) rightRotorTurn = Rotor3Turn;
+	else if(rightRotor==4) rightRotorTurn = Rotor4Turn;
+	else if(rightRotor==5) rightRotorTurn = Rotor5Turn;
+	else if(rightRotor==6) rightRotorTurn = Rotor6Turn;
+	else if(rightRotor==7) rightRotorTurn = Rotor7Turn;
+	else rightRotorTurn = Rotor8Turn;
+		
 	char[] input = inputString.toCharArray();
 	char[] output = new char[inputString.length()];
-
 	
 	for(int i=0; i<inputString.length(); i++)
-	{
-		boolean middleTurned = false;
-		rightGround= (rightGround+1)%26;
-		if(i!=0 && turnMiddle(rightRotor, rightGround)) 
-		{
-			middleGround = (middleGround+1)%26; 
-			middleTurned = true;
-		}
-		if(middleTurned && turnLeft(middleRotor, middleGround)) leftGround = (leftGround+1)%26;
+	{	
+		//regular turning spots	
+		if(rightGround == rightRotorTurn[0] || rightGround == rightRotorTurn[1]){
+			if(middleGround == middleRotorTurn[0] || middleGround == middleRotorTurn[1]){
+				leftGround = (leftGround + 1) % 26;
+				}
+		middleGround = (middleGround + 1) % 26;
 		
-		System.out.println("After the " +  (i+1) +"'th letter grund settings are: "+leftGround+" "+middleGround+" "+rightGround);
+		}
+		else{
+			//double step
+			if(middleGround == middleRotorTurn[0] || middleGround == middleRotorTurn[1]){
+				leftGround = (leftGround + 1) % 26;
+				middleGround = (middleGround + 1) % 26;
+				}
+		}
+		
+		//turn for right rotor each run
+		rightGround = (rightGround + 1) % 26;
+				
 		int c = charToInt(input[i]);
-		c = rotor(c, rightRotor, rightGround);
-		c = rotor(c, middleRotor, middleGround);
-		c = rotor(c, leftRotor, leftGround);
+		c = PlugSwap(c);
+		c = rotor(c, rightRotorArray, rightGround, rightRing);
+		c = rotor(c, middleRotorArray, middleGround, middleRing);
+		c = rotor(c, leftRotorArray, leftGround, leftRing);
 		c = ReflectorB(c);
-		c = rotorInverse(c, leftRotor, leftGround);
-		c = rotorInverse(c, middleRotor, middleGround);
-		c = rotorInverse(c, rightRotor, rightGround);
+		c = rotorInverse(c, leftRotorArray, leftGround, leftRing);
+		c = rotorInverse(c, middleRotorArray, middleGround, middleRing);
+		c = rotorInverse(c, rightRotorArray, rightGround, rightRing);
+		c = PlugSwap(c);
+		
 		output[i] = intToChar(c);
+	
 	}
 	String outputString = new String(output);
 	return outputString;
+	
 }
 	
-public static int rotor(int inputChar, int rotorNumber, int GrundSetting){
-	int[] inputRotor;
-	if(rotorNumber==1) inputRotor = Rotor1Int; 
-	else if(rotorNumber==2) inputRotor = Rotor2Int;
-	else if(rotorNumber==3) inputRotor = Rotor3Int;
-	else if(rotorNumber==4) inputRotor = Rotor4Int;
-	else if(rotorNumber==5) inputRotor = Rotor5Int;
-	else if(rotorNumber==6) inputRotor = Rotor6Int;
-	else if(rotorNumber==7) inputRotor = Rotor7Int;
-	else inputRotor = Rotor8Int;
+public static int rotor(int inputChar, int[] inputRotor, int GrundSetting, int RingSetting){
 	//takes value and adds the grund shift value.
-	int AdjustedValue = (inputChar + GrundSetting) % 26; 
+	int AdjustedValue = ((inputChar + GrundSetting) - RingSetting + 26) % 26;
 	//takes output from array and subtracts grund shift value.
-	if(inputRotor[AdjustedValue] - GrundSetting >0)
-		return (inputRotor[AdjustedValue] - GrundSetting) % 26;
-	else return (25 + (inputRotor[AdjustedValue] - GrundSetting));
+	int ReturnValue = (inputRotor[AdjustedValue] - GrundSetting + RingSetting + 26) % 26;
+	
+	return ReturnValue;
 }
 	
-public static int rotorInverse(int inputChar, int rotorNumber, int grundSetting){
-    char[] Default = new char[]{'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
-    int[] DefaultInt = new int[26];
-    for(int a = 0; a < 26 ; a++){
-        DefaultInt[a] = charToInt(Default[a]);
-    }
-    
-    //finds the correct rotor
-    int[] inputRotor;
-    if(rotorNumber==1) inputRotor = Rotor1Int; 
-    else if(rotorNumber==2) inputRotor = Rotor2Int;
-    else if(rotorNumber==3) inputRotor = Rotor3Int;
-    else if(rotorNumber==4) inputRotor = Rotor4Int;
-    else if(rotorNumber==5) inputRotor = Rotor5Int;
-    else if(rotorNumber==6) inputRotor = Rotor6Int;
-    else if(rotorNumber==7) inputRotor = Rotor7Int;
-    else inputRotor = Rotor8Int;
- 
-    //takes value and subtracts the grund shift value.
-    int AdjustedValue = (inputChar + grundSetting) % 26;
-            
-    int index = 0;
-    while(inputRotor[index] != AdjustedValue){
-        index++;
-    }
-    
-    index = index - grundSetting;
-    if (index < 0){
-        index = index + 26;
-    }
-    
-    //takes output from array and subtracts grund shift value.
-    return DefaultInt[index];
+public static int rotorInverse(int inputChar, int[] inputRotor, int grundSetting, int RingSetting){
+	char[] Default = new char[]{'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+	int[] DefaultInt = new int[26];
+	for(int a = 0; a < 26 ; a++){
+		DefaultInt[a] = charToInt(Default[a]);
+	}
+		
+	//takes value and subtracts the grund shift value.
+	int AdjustedValue = ((inputChar + grundSetting - RingSetting) + 26) % 26;
+			
+	int index = 0;
+	while(inputRotor[index] != AdjustedValue){
+		index++;
+	}
+	
+	index = (index - grundSetting + RingSetting + 26) % 26;
+	
+	//takes output from array and subtracts grund shift value.
+	return DefaultInt[index];
 }
+	
 
 public static int ReflectorB(int inputChar){	
 	return ReflectorInt[inputChar];
@@ -238,14 +282,14 @@ public static boolean checkInput(char Input){
 	return Check;
 }
 	
-	//Converts input into the plug swapped value.
-	public static char PlugSwap(char Input){
-		return Plug[charToInt(Input)];
-	}
+//Converts input into the plug swapped value.
+public static int PlugSwap(int Input){
+		return charToInt(Plug[Input]);
+}
 	
-	//Takes two inputs and swaps them. Edits the array. (A would be 0 so if you swap A to Z, it would set Plug[0] to Z and Plug[25] to A)
-	//By default they are in normal order. (A -> Z)
-	public static void PlugSetup(char Input1, char Input2){
+//Takes two inputs and swaps them. Edits the array. (A would be 0 so if you swap A to Z, it would set Plug[0] to Z and Plug[25] to A)
+//By default they are in normal order. (A -> Z)
+public static void PlugSetup(char Input1, char Input2){
 		if(checkInput(Input1) && checkInput(Input2)){
 			int InputOne = charToInt(Input1);
 			int InputTwo = charToInt(Input2);
@@ -259,8 +303,9 @@ public static boolean checkInput(char Input){
 	}
 
 /**
+ * @deprecated Stan's old implementation of rotation -- Kevin made better implementation
  * decides whether the middle rotor should be turn based on the number and position of right rotor
- * @return
+ * @return whether or not middle rotor should be rotated 
  */
 private static boolean turnMiddle(int rightRotor, int position)
 {
@@ -275,8 +320,9 @@ private static boolean turnMiddle(int rightRotor, int position)
 }
 
 /**
+ * @deprecated
  * decides whether the left rotor should be turn based on the number and position of middle rotor
- * @return
+ * @return whether or not middle rotor should be rotated
  */
 private static boolean turnLeft(int middleRotor, int position)
 {
